9d8e1621f37117f237a43ced9bbebc804c09bd27
Henrik Johansson <khvjohansson@gmail.com>
2019-09-11 01:34:22 +0200

    added some fun statistics that dont crash the page

--- tale_maps/src/views/Landing.vue
+++ tale_maps/src/views/Landing.vue
@@ -1,9 +1,22 @@
 <template>
     <div>
-        <div>{{$store.state.logs}}</div>
+        <h1>Your stats</h1>
+        <div class="container">
+            <ul style="list-style:none;">
+                <li>Have {{$store.state.logs.logData.length}} log entries</li>
+                <li>You have died {{$store.state.logs.logData.filter(x => x[5] == 301).length}} times</li>
+                <li>Finished {{$store.state.logs.logData.filter(x => x[5] == 311).length}} quests</li>
+                <li>Killed {{$store.state.logs.logData.filter(x => x[5] == 401).length}} mobs</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
+.container {
+    background-color: gray;
+    padding: 2em;
+}
+
 </style>
